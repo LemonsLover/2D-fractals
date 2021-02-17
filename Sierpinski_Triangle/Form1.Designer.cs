e6d725df94d8acc563cc19795dde31590f05ba75
Yehor Ovseukov <ki201_oeyu@student.ztu.edu.ua>
2021-02-17 15:38:30 +0200

    first commit

---
+++ Sierpinski_Triangle/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace Sierpinski_Triangle
+{
+    partial class mainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainScreen));
+            this.pictureBoxScreen = new System.Windows.Forms.PictureBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.numericUpDownDotWidth = new System.Windows.Forms.NumericUpDown();
+            this.labelDotWidth = new System.Windows.Forms.Label();
+            this.timerDraw = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.pictureBoxLine = new System.Windows.Forms.PictureBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.numericUpDownDotSpeed = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelDotAmount = new System.Windows.Forms.Label();
+            this.numericUpDownDotAmount = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxRndColors = new System.Windows.Forms.CheckBox();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.checkBoxOneATick = new System.Windows.Forms.CheckBox();
+            this.checkBoxRndFirstDot = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxScreen
+            // 
+            resources.ApplyResources(this.pictureBoxScreen, "pictureBoxScreen");
+            this.pictureBoxScreen.Name = "pictureBoxScreen";
+            this.pictureBoxScreen.TabStop = false;
+            this.pictureBoxScreen.Resize += new System.EventHandler(this.pictureBoxScreen_Resize);
+            // 
+            // buttonDraw
+            // 
+            resources.ApplyResources(this.buttonDraw, "buttonDraw");
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // numericUpDownDotWidth
+            // 
+            resources.ApplyResources(this.numericUpDownDotWidth, "numericUpDownDotWidth");
+            this.numericUpDownDotWidth.DecimalPlaces = 1;
+            this.numericUpDownDotWidth.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDownDotWidth.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownDotWidth.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            65536});
+            this.numericUpDownDotWidth.Name = "numericUpDownDotWidth";
+            this.numericUpDownDotWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDotWidth.ValueChanged += new System.EventHandler(this.numericUpDownDotWidth_ValueChanged);
+            // 
+            // labelDotWidth
+            // 
+            resources.ApplyResources(this.labelDotWidth, "labelDotWidth");
+            this.labelDotWidth.Name = "labelDotWidth";
+            // 
+            // timerDraw
+            // 
+            this.timerDraw.Interval = 1;
+            this.timerDraw.Tick += new System.EventHandler(this.timerDraw_Tick);
+            // 
+            // buttonStop
+            // 
+            resources.ApplyResources(this.buttonStop, "buttonStop");
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonClean
+            // 
+            resources.ApplyResources(this.buttonClean, "buttonClean");
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
+            // comboBoxColor
+            // 
+            resources.ApplyResources(this.comboBoxColor, "comboBoxColor");
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Items.AddRange(new object[] {
+            resources.GetString("comboBoxColor.Items"),
+            resources.GetString("comboBoxColor.Items1"),
+            resources.GetString("comboBoxColor.Items2"),
+            resources.GetString("comboBoxColor.Items3"),
+            resources.GetString("comboBoxColor.Items4")});
+            this.comboBoxColor.Name = "comboBoxColor";
+            // 
+            // labelColor
+            // 
+            resources.ApplyResources(this.labelColor, "labelColor");
+            this.labelColor.Name = "labelColor";
+            // 
+            // pictureBoxLine
+            // 
+            resources.ApplyResources(this.pictureBoxLine, "pictureBoxLine");
+            this.pictureBoxLine.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBoxLine.Name = "pictureBoxLine";
+            this.pictureBoxLine.TabStop = false;
+            // 
+            // labelTitle
+            // 
+            resources.ApplyResources(this.labelTitle, "labelTitle");
+            this.labelTitle.Name = "labelTitle";
+            // 
+            // numericUpDownDotSpeed
+            // 
+            resources.ApplyResources(this.numericUpDownDotSpeed, "numericUpDownDotSpeed");
+            this.numericUpDownDotSpeed.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.Maximum = new decimal(new int[] {
+            -1675358184,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.Name = "numericUpDownDotSpeed";
+            this.numericUpDownDotSpeed.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.ValueChanged += new System.EventHandler(this.numericUpDownDotSpeed_ValueChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // labelDotAmount
+            // 
+            resources.ApplyResources(this.labelDotAmount, "labelDotAmount");
+            this.labelDotAmount.Name = "labelDotAmount";
+            // 
+            // numericUpDownDotAmount
+            // 
+            resources.ApplyResources(this.numericUpDownDotAmount, "numericUpDownDotAmount");
+            this.numericUpDownDotAmount.Maximum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            0});
+            this.numericUpDownDotAmount.Name = "numericUpDownDotAmount";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // checkBoxRndColors
+            // 
+            resources.ApplyResources(this.checkBoxRndColors, "checkBoxRndColors");
+            this.checkBoxRndColors.Name = "checkBoxRndColors";
+            this.checkBoxRndColors.UseVisualStyleBackColor = true;
+            // 
+            // labelSpeed
+            // 
+            resources.ApplyResources(this.labelSpeed, "labelSpeed");
+            this.labelSpeed.Name = "labelSpeed";
+            // 
+            // checkBoxOneATick
+            // 
+            resources.ApplyResources(this.checkBoxOneATick, "checkBoxOneATick");
+            this.checkBoxOneATick.Name = "checkBoxOneATick";
+            this.checkBoxOneATick.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRndFirstDot
+            // 
+            resources.ApplyResources(this.checkBoxRndFirstDot, "checkBoxRndFirstDot");
+            this.checkBoxRndFirstDot.Name = "checkBoxRndFirstDot";
+            this.checkBoxRndFirstDot.UseVisualStyleBackColor = true;
+            // 
+            // mainScreen
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.checkBoxRndFirstDot);
+            this.Controls.Add(this.checkBoxOneATick);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.checkBoxRndColors);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelDotAmount);
+            this.Controls.Add(this.numericUpDownDotAmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownDotSpeed);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.pictureBoxLine);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.comboBoxColor);
+            this.Controls.Add(this.buttonClean);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelDotWidth);
+            this.Controls.Add(this.numericUpDownDotWidth);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.pictureBoxScreen);
+            this.Name = "mainScreen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxScreen;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.NumericUpDown numericUpDownDotWidth;
+        private System.Windows.Forms.Label labelDotWidth;
+        private System.Windows.Forms.Timer Draw_Tick;
+        private System.Windows.Forms.Timer timerDraw;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonClean;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.PictureBox pictureBoxLine;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.NumericUpDown numericUpDownDotSpeed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelDotAmount;
+        private System.Windows.Forms.NumericUpDown numericUpDownDotAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxRndColors;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.CheckBox checkBoxOneATick;
+        private System.Windows.Forms.CheckBox checkBoxRndFirstDot;
+    }
+}
+
---
+++ Sierpinski_Triangle/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Sierpinski_Triangle
+{
+    public partial class mainScreen : Form
+    {
+        public float dotWidth;
+        public double dotAmount = 0;
+        public int dotATick;
+
+        public mainScreen()
+        {
+            InitializeComponent();
+        }
+
+
+        List<PointF> Corners;
+
+        PointF LastPoint;
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            DefineCorners();
+            if (numericUpDownDotAmount.Value == 0)
+                timerDraw.Enabled = true;
+            else
+                timerDraw_Tick(sender, e);
+            dotWidth = Convert.ToInt64(numericUpDownDotWidth.Value);
+            Pen pen = new Pen(Color.Brown, dotWidth);
+            using (Graphics gr = pictureBoxScreen.CreateGraphics())
+            {
+                gr.DrawEllipse(pen, LastPoint.X, LastPoint.Y, dotWidth, dotWidth);
+            }
+        }
+
+        private void DefineCorners()
+        {
+            Random rand = new Random();
+            Corners = new List<PointF>();
+            Corners.Add(new PointF(pictureBoxScreen.Width / 2, 10));
+            Corners.Add(new PointF(10, pictureBoxScreen.Height - 10));
+            Corners.Add(new PointF(pictureBoxScreen.Width - 10, pictureBoxScreen.ClientSize.Height - 10));
+
+            if (!checkBoxRndFirstDot.Checked)
+                LastPoint = Corners[0];
+            else if(checkBoxRndFirstDot.Checked)
+                LastPoint = new PointF(rand.Next(0, pictureBoxScreen.Width/2), rand.Next(0, pictureBoxScreen.Height/2));
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DefineCorners();
+            numericUpDownDotSpeed_ValueChanged(sender, e);
+            comboBoxColor.SelectedIndex = 0;
+            dotAmount = 0;
+            if (checkBoxOneATick.Checked)
+                dotATick = 1;
+            else
+                dotATick = 1000;
+        }
+
+        private void timerDraw_Tick(object sender, EventArgs e)
+        {
+            if (checkBoxOneATick.Checked)
+                dotATick = 1;
+            else
+                dotATick = 1000;
+            Random rand = new Random();
+            using (Graphics gr = pictureBoxScreen.CreateGraphics())
+            {
+                Pen pen;
+                if (numericUpDownDotAmount.Value == 0)
+                    for (int i = 1; i <= dotATick; i++)
+                    {
+                    int j = rand.Next(0, 3);
+                    LastPoint = new PointF(
+                        (LastPoint.X + Corners[j].X) / 2,
+                        (LastPoint.Y + Corners[j].Y) / 2);
+                        if (!checkBoxRndColors.Checked)
+                            pen = new Pen(Color.FromName(comboBoxColor.SelectedItem.ToString()), dotWidth);
+                        else
+                            pen = new Pen(Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255)), dotWidth);
+                        gr.DrawEllipse(pen, LastPoint.X, LastPoint.Y,
+                        dotWidth, dotWidth);
+                        dotAmount ++;
+                        this.Text = $"Треугольник Серпинского(Точек на экране: {dotAmount})";
+                    }
+                else
+                    for (int i = 1; i <= numericUpDownDotAmount.Value; i++)
+                    {
+                        int j = rand.Next(0, 3);
+                        LastPoint = new PointF(
+                            (LastPoint.X + Corners[j].X) / 2,
+                            (LastPoint.Y + Corners[j].Y) / 2);
+                        if (!checkBoxRndColors.Checked)
+                            pen = new Pen(Color.FromName(comboBoxColor.SelectedItem.ToString()), dotWidth);
+                        else
+                            pen = new Pen(Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255)), dotWidth);
+                        gr.DrawEllipse(pen, LastPoint.X, LastPoint.Y,
+                            dotWidth, dotWidth);
+                        dotAmount++;
+                        this.Text = $"Треугольник Серпинского(Точек на экране: {dotAmount})";
+                    }
+            }
+        }
+
+        private void pictureBoxScreen_Resize(object sender, EventArgs e)
+        {
+            timerDraw.Enabled = false;
+            DefineCorners();
+
+            //using (Graphics gr = pictureBoxScreen.CreateGraphics())
+            //{
+            //    gr.Clear(this.BackColor);
+            //}
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timerDraw.Enabled = false;
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e)
+        {
+            using (Graphics gr = pictureBoxScreen.CreateGraphics())
+            {
+                gr.Clear(this.BackColor);
+            }
+            timerDraw.Enabled = false;
+            dotAmount = 0;
+            this.Text = $"Треугольник Серпинского(Точек на экране: {dotAmount})";
+        }
+
+        private void numericUpDownDotSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            timerDraw.Interval = Convert.ToInt16(numericUpDownDotSpeed.Value);
+        }
+
+        private void numericUpDownDotWidth_ValueChanged(object sender, EventArgs e)
+        {
+            dotWidth = Convert.ToInt64(numericUpDownDotWidth.Value);
+        }
+    }
+}
